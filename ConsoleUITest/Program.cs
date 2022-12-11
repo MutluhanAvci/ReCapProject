6bf0aed55110d53eb1423bb84adfde15c5372138
Mutluhan Avci <65674782+MutluhanAvci@users.noreply.github.com>
2022-12-12 00:19:56 +0300

    BrandManager added

--- ConsoleUITest/Program.cs
+++ ConsoleUITest/Program.cs
@@ -1,6 +1,7 @@
 ﻿using Business.Concrete;
 using DataAccess.Abstract;
 using DataAccess.Concrete.InMemory;
+using Entities.Concrete;
 using System;
 
 namespace ConsoleUITest
@@ -14,6 +15,14 @@ namespace ConsoleUITest
             foreach (var item in carManager.GetAll())
             {
                 Console.WriteLine(item.Description);
+            }
+            Car car = new Car { BrandId = 1, CategoryId = 2, ColorId = 1, DailyPrice = 1050, Description = "E250", Id = 10, ModelYear = 2001 };
+            BrandManager brandManager = new BrandManager(new InMemoryBrandDal());
+            brandManager.GetBrandName(car);
+            foreach (var item in brandManager.GetBrandName(car))
+            
+            {
+                Console.WriteLine(item.BrandName+"----------");
             }
             
         }
---
+++ DataAccess/Concrete/InMemory/InMemoryBrandDal.cs
@@ -0,0 +1,28 @@
+﻿using DataAccess.Abstract;
+using Entities.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataAccess.Concrete.InMemory
+{
+    public class InMemoryBrandDal : IBrandDal
+    {
+        List<Brand> _brands;
+        public InMemoryBrandDal()
+        {
+            _brands = new List<Brand>
+            {
+               new Brand{BrandId=1,BrandName="Toyota"},
+               new Brand{BrandId=2,BrandName="Opel"},
+               new Brand{BrandId=3,BrandName="Mercedes"},
+               new Brand{BrandId=4,BrandName="BMW"},
+            };
+        }
+        public List<Brand> GetBrandNameByCar(Car car)
+        {
+            return _brands.Where(b => b.BrandId == car.BrandId).ToList();
+        }
+    }
+}
